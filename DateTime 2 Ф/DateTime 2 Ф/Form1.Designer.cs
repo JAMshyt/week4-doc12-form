dc048691f0b26420e3ad328e8e9605ed79e6cace
JAMshyt <77636014+JAMshyt@users.noreply.github.com>
2022-12-20 09:58:14 +0300

    Add files via upload

---
+++ DateTime 2 Ф/DateTime 2 Ф/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace DateTime_2_Ф
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+			this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+			this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+			this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+			this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// dateTimePicker1
+			// 
+			this.dateTimePicker1.Location = new System.Drawing.Point(12, 12);
+			this.dateTimePicker1.Name = "dateTimePicker1";
+			this.dateTimePicker1.Size = new System.Drawing.Size(142, 20);
+			this.dateTimePicker1.TabIndex = 0;
+			// 
+			// numericUpDown1
+			// 
+			this.numericUpDown1.Location = new System.Drawing.Point(363, 8);
+			this.numericUpDown1.Maximum = new decimal(new int[] {
+            9999,
+            0,
+            0,
+            0});
+			this.numericUpDown1.Minimum = new decimal(new int[] {
+            9999,
+            0,
+            0,
+            -2147483648});
+			this.numericUpDown1.Name = "numericUpDown1";
+			this.numericUpDown1.Size = new System.Drawing.Size(142, 20);
+			this.numericUpDown1.TabIndex = 1;
+			// 
+			// richTextBox1
+			// 
+			this.richTextBox1.Location = new System.Drawing.Point(12, 77);
+			this.richTextBox1.Name = "richTextBox1";
+			this.richTextBox1.Size = new System.Drawing.Size(329, 163);
+			this.richTextBox1.TabIndex = 2;
+			this.richTextBox1.Text = "";
+			// 
+			// richTextBox2
+			// 
+			this.richTextBox2.Location = new System.Drawing.Point(363, 73);
+			this.richTextBox2.Name = "richTextBox2";
+			this.richTextBox2.Size = new System.Drawing.Size(142, 93);
+			this.richTextBox2.TabIndex = 3;
+			this.richTextBox2.Text = "";
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(12, 246);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(142, 19);
+			this.button1.TabIndex = 4;
+			this.button1.Text = "button1";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(363, 34);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(142, 33);
+			this.button2.TabIndex = 5;
+			this.button2.Text = "button2";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(13, 34);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(9, 13);
+			this.label1.TabIndex = 6;
+			this.label1.Text = "i";
+			// 
+			// numericUpDown2
+			// 
+			this.numericUpDown2.Location = new System.Drawing.Point(16, 51);
+			this.numericUpDown2.Maximum = new decimal(new int[] {
+            999,
+            0,
+            0,
+            0});
+			this.numericUpDown2.Minimum = new decimal(new int[] {
+            999,
+            0,
+            0,
+            -2147483648});
+			this.numericUpDown2.Name = "numericUpDown2";
+			this.numericUpDown2.Size = new System.Drawing.Size(120, 20);
+			this.numericUpDown2.TabIndex = 7;
+			// 
+			// dateTimePicker2
+			// 
+			this.dateTimePicker2.Location = new System.Drawing.Point(184, 12);
+			this.dateTimePicker2.Name = "dateTimePicker2";
+			this.dateTimePicker2.Size = new System.Drawing.Size(142, 20);
+			this.dateTimePicker2.TabIndex = 8;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(517, 277);
+			this.Controls.Add(this.dateTimePicker2);
+			this.Controls.Add(this.numericUpDown2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.richTextBox2);
+			this.Controls.Add(this.richTextBox1);
+			this.Controls.Add(this.numericUpDown1);
+			this.Controls.Add(this.dateTimePicker1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+		private System.Windows.Forms.DateTimePicker dateTimePicker2;
+	}
+}
+
---
+++ DateTime 2 Ф/DateTime 2 Ф/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DateTime_2_Ф {
+	public partial class Form1 : Form {
+		public Form1() {
+			InitializeComponent();
+		}
+
+		class Date {
+			public DateTime date;
+
+			public Date() {
+				DateTime data = new DateTime(2009, 01, 1);
+			}
+			public void Enter(DateTime a) {
+				date = a;
+			}
+
+			public string days(DateTime a) {
+				DateTime now = a;
+				DateTime yesterday = now.AddDays(-1);
+				DateTime tomorrow = now.AddDays(1);
+				int daysLeft = DateTime.DaysInMonth(now.Year, now.Month) - now.Day;
+				return ""
+					+ "\nВчера: " + yesterday.ToString("dd.MM.yyyy")
+					+ "\nЗавтра: " + tomorrow.ToString("dd.MM.yyyy")
+					+ "\nОсталось дней до конца месяца: " + daysLeft;
+			}
+
+			public string leap(int a) {
+				string yes;
+				bool lea = DateTime.IsLeapYear(Convert.ToInt32(a));
+				if (lea == true) { yes = "Высокосный"; } else { yes = "Не высокосный"; }
+				return yes;
+			}
+
+			public DateTime this[DateTime da, int index] {
+				get {
+					return da.AddDays(index);
+				}
+			}
+
+			public static bool operator !(Date a) {
+				return a.date.Day != DateTime.DaysInMonth(a.date.Year, a.date.Month);
+			}
+
+			public static bool operator true(Date a) {
+				return a.date.Day == 1 && a.date.Month == 1;
+			}
+
+			public static bool operator false(Date a) {
+				return a.date.Day != 1 || a.date.Month != 1;
+			}
+
+			public static bool operator &(Date a, Date b) {
+				return a.date.Equals(b.date);
+			}
+
+			public static explicit operator String(Date day) {
+				return day.date.Day + "." + day.date.Month + "." + day.date.Year;
+			}
+
+			public Date(int year, int month, int day) {
+				date = new DateTime(year, month, day);
+			}
+
+			public static explicit operator Date(String str) {
+				string[] str_arr = str.Split(new char[] { ' ', ',', '/', '.' }, StringSplitOptions.RemoveEmptyEntries);
+				Date obj = new Date(Convert.ToInt32(str_arr[0]), Convert.ToInt32(str_arr[1]), Convert.ToInt32(str_arr[2]));
+				return obj;
+			}
+		}
+
+
+
+		private void button1_Click(object sender, EventArgs e) {
+			richTextBox1.Text = "";
+			Date d = new Date();
+			d.Enter(dateTimePicker1.Value);
+			richTextBox1.Text += d.days(dateTimePicker1.Value) + "\n";
+			int N = Convert.ToInt32(numericUpDown2.Value);
+			richTextBox1.Text += "Будет: " + d[dateTimePicker1.Value, N].ToString("dd.MM.yyyy") + "\n";
+			richTextBox1.Text += "Преобразовани  DataTime в string: " + (string)d;
+			Date d2 = (Date)d;
+			richTextBox1.Text += "\nПреобразования класса string в тип DataTime: " + d2.date.ToShortDateString() + "\n";
+			richTextBox1.Text += "Дата не является последним днём месяца: " + !d + "\n";
+			Date b = new Date();
+			b.Enter(dateTimePicker2.Value);
+			richTextBox1.Text += "Сравнение дат: " + (d & b) + "\n";
+			if (d) {
+				richTextBox1.Text += "Начало года";
+			}
+			else { richTextBox1.Text += "Не начало года"; }
+
+
+		}
+
+		private void button2_Click(object sender, EventArgs e) {
+			if (numericUpDown1.Value <= 0) {
+				MessageBox.Show("год должен быть больше 0");
+			}
+			else {
+				Date d = new Date();
+				try { richTextBox2.Text = d.leap(Convert.ToInt32(numericUpDown1.Value)); }
+				catch { MessageBox.Show("Год должен быть больше 0"); }
+			}
+		}
+	}
+}
